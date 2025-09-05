c026080b2bce4b5bf988517c1a622f125563a790
ldotmithu <ldotmithurshan222@gmail.com>
2025-09-05 23:34:19 +0530

    updated folder struture

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name="C-RAG",
+      version="0.0.0",
+      author="ldotmithurshan",
+      author_email="ldotmithurshan222@gmail.com",
+      packages=find_packages()
+      )
